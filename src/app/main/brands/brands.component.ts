b0476898ee7df215ad3308907580c2100780a9d1
ismailharik <ismailharik77@gmail.com>
2024-06-13 19:21:49 +0100

    add content of models

--- src/app/main/brands/brands.component.ts
+++ src/app/main/brands/brands.component.ts
@@ -14,30 +14,37 @@ export class BrandsComponent {
     this.slidesStore = [
       {
         id : 1,
-        src: "assets/img/brand-1.svg",
-        alt: "brand-1",
-        title: "brand-1",
+        src: "assets/img/brand-6.png",
+        alt: "brand-6",
+        title: "brand-6",
       },
+
       {
         id : 2,
-        src: "assets/img/brand-2.jpg",
+        src: "assets/img/brand-1.svg",
         alt: "brand-2",
         title: "brand-2",
       },
       {
         id : 3,
+        src: "assets/img/brand-2.jpg",
+        alt: "brand-2",
+        title: "brand-2",
+      },
+      {
+        id : 4,
         src: "assets/img/brand-3.png",
         alt: "brand-3",
         title: "brand-3",
       },
       {
-        id : 4,
+        id : 5,
         src: "assets/img/brand-4.png",
         alt: "brand-4",
         title: "brand-4",
       },
       {
-        id : 5,
+        id : 6,
         src: "assets/img/brand-5.jpg",
         alt: "brand-1",
         title: "brand-1",
